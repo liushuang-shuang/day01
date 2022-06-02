1b42ac61d7387b1686ef15aa87bcca20f7eaaf9e
柳爽 <ls822318@163.com>
2022-06-02 09:48:06 +0800

    新建

---
+++ src/main/java/com/atguigu/Hello.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author
+ * @Description
+ * @date
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("新建文件");
+    }
+}
