56cdc1616b099e225e8a3139109283f2b0a30251
柳爽 <ls822318@163.com>
2022-06-02 09:52:17 +0800

    第二次更新

--- src/main/java/com/atguigu/Hello.java
+++ src/main/java/com/atguigu/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("新建文件");
         System.out.println("第一次更新");
+        System.out.println("第二次更新");
     }
 }
